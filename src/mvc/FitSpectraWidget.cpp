282fb276e22e9b05709a45b22716ed13c98df335
Arthur Glowacki <aglowakci@anl.gov>
2024-08-13 13:11:46 -0500

    Move mpfit above lmfit in the combobox

--- src/mvc/FitSpectraWidget.cpp
+++ src/mvc/FitSpectraWidget.cpp
@@ -228,8 +228,8 @@ void FitSpectraWidget::createLayout()
 
     _cb_opttimizer = new QComboBox();
     _cb_opttimizer->addItem(STR_HYBRID_MP_FIT);
-    _cb_opttimizer->addItem(STR_LM_FIT);
     _cb_opttimizer->addItem(STR_MP_FIT);
+    _cb_opttimizer->addItem(STR_LM_FIT);
     connect(_cb_opttimizer, &QComboBox::currentTextChanged, this, &FitSpectraWidget::optimizer_changed);
 
     _chk_auto_model = new QCheckBox("Auto Update Model");
